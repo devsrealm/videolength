bf4ee32ff8cb662361170f2556cfb1c0631cf67c
Ahmed Faruq <37757164+devsrealm@users.noreply.github.com>
2021-01-26 15:24:27 +0100

    Update script.js

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,11 +1,14 @@
- document.getElementById('videoInput').onchange  = function(){
+document.getElementById('videoInput').onchange  = function(){
     const file = document.getElementById('videoInput').files[0];
     const reader = new FileReader();
     reader.onload = function() {
         const audio = new Audio(reader.result);
         audio.onloadedmetadata = function(){
             // toFixed(2) rounds the number to keep only two decimals e.g .44
-            const dur = audio.duration.toFixed(2); // Get The Duration
+            let minutes = parseInt(audio.duration / 60, 10);
+            let seconds = parseInt(audio.duration % 60);
+
+            const dur = `${minutes}:${seconds}`; // Get The Duration
             // replace the dot(.) to column(:)
             document.getElementById('videolength_duration').value = dur.replace(".", ":");
         };
